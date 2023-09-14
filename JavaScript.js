c61d2e7d21428373442e97b27c39afb2b919e82b
Ruebeb07 <135453157+Ruebeb07@users.noreply.github.com>
2023-09-14 20:09:27 +0200

    Update JavaScript.js
    fixed:
    - bmi wordt afgerond
    
    added:
    - bmi wordt niet meer in een balkje weergegeven, maar in een tekst

--- JavaScript.js
+++ JavaScript.js
@@ -1,33 +1,34 @@
-      //defining variables
-      var BerekeningenContent = document.getElementsByClassName("")
+//defining variables
+var BerekeningenContent = document.getElementsByClassName("")
 
-      //function to dropdown the contents of the dropdown menu
-      function DropDownBerekening(){
-        console.log("Function called")
-        if (BerekeningenContent.style.display == "none;"){
-          BerekeningenContent.style.display = "initial;";
-        }
-        else{
-          BerekeningenContent.style.display = "none;"
-        }
-      }
-      function BMIberekenFunction() {
-        var length = document.getElementById("length").value;
-        var weigth = document.getElementById("weigth").value;
-            
-        if (length < 65) {
-                length=NaN
-        }
-        else if  (length > 252){
-                length=NaN
-        }
-        else if (weigth > 595){
-                weigth=NaN
-        }
-        else if (weigth < 0){
-                weigth=NaN
-        }
-        else {
-        document.getElementById ("result").value = ((weigth / (length*length))*10000);
-        }
-      }
+//function to dropdown the contents of the dropdown menu
+function DropDownBerekening(){
+  console.log("Function called")
+  if (BerekeningenContent.style.display == "none;"){
+    BerekeningenContent.style.display = "initial;";
+  }
+  else{
+    BerekeningenContent.style.display = "none;"
+  }
+}
+function BMIberekenFunction() {
+  var length = document.getElementById("length").value;
+  var weigth = document.getElementById("weigth").value;
+          
+  if (length < 65) {
+    length=NaN
+  }
+  else if  (length > 252){
+    length=NaN
+  }
+  else if (weigth > 595){
+    weigth=NaN
+  }
+  else if (weigth < 0){
+    weigth=NaN
+  }
+  else {
+    var BMI = ((weigth / (length*length))*10000)
+    document.getElementById ("result").innerHTML = ("BMI = "+ String(BMI.toFixed(2)));
+  }
+}
